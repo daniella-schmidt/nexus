40c844b857a04f8f5413d288a717be70483ac452
daniella-schmidt <daniella.schmidt@unoesc.edu.br>
2025-11-10 20:00:16 -0300

    banco ajustado para teste

--- nexus.sql
+++ nexus.sql
@@ -18,6 +18,8 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 -- Banco de dados: `nexus`
+CREATE DATABASE IF NOT EXISTS `nexus` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `nexus`;
 
 -- --------------------------------------------------------
 
